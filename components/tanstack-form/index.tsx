e101c296e56a0476e61c5e6b3136a94005d8fb4d
tannertanner1 <tannerserson@gmail.com>
2025-08-06 23:57:10 -0700

    feat(form): add withForm and withFieldGroup

--- components/tanstack-form/index.tsx
+++ components/tanstack-form/index.tsx
@@ -5,7 +5,7 @@ import { Button } from "./button"
 export const { fieldContext, useFieldContext, formContext, useFormContext } =
   createFormHookContexts()
 
-export const { useAppForm } = createFormHook({
+export const { useAppForm, withForm, withFieldGroup } = createFormHook({
   fieldComponents: {
     Input,
     Select,
